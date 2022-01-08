66ba6df2f256243d89ac3536d89b62d016e0535a
Prashant Shrestha <contact@prashant.me>
2022-01-08 15:39:15 -0500

    Done with variables, onto loops.

---
+++ typescript/variables.ts
@@ -0,0 +1,9 @@
+let mastered: boolean = false;
+const sayTypeScript: string = "This is Typescript!";
+var anyTypeVariable: any = false;
+
+console.log(`Mastered TS yet? ${mastered}\n${sayTypeScript}`);
+anyTypeVariable = 0.01015123;
+console.log(`Any type variable value = ${anyTypeVariable}.`);
+anyTypeVariable = "Hello!";
+console.log(`Any type variable value = ${anyTypeVariable}.`);
