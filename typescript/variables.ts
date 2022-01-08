ca14cd9e9c5f760ed6539c9e688de1efd5c6e06b
Prashant Shrestha <contact@prashant.me>
2022-01-08 15:42:11 -0500

    Added collection example (array).

--- typescript/variables.ts
+++ typescript/variables.ts
@@ -1,9 +1,14 @@
 let mastered: boolean = false;
 const sayTypeScript: string = "This is Typescript!";
 var anyTypeVariable: any = false;
+var nums: number[] = [5, 5, 5, 5, 11, 1, 1, 3, 4, 5, 6];
+let reviews: string[] = ["Bad review", "Good Review"]
 
 console.log(`Mastered TS yet? ${mastered}\n${sayTypeScript}`);
 anyTypeVariable = 0.01015123;
 console.log(`Any type variable value = ${anyTypeVariable}.`);
 anyTypeVariable = "Hello!";
 console.log(`Any type variable value = ${anyTypeVariable}.`);
+console.log(`Array of numbers = ${nums}`);
+reviews.push("Average review..");
+console.log(`Reviews => ${reviews.join(", ")}`);
